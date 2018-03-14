f3648081f84fac6b8cbd637b0584edf42dbc978e
Colin Adams <colin.adams@actual-experience.com>
2018-03-14 15:16:52 +0000

    covered all lines in all function sin calculate.js

--- test/calculate/caclulate-epoch-ms-test.js
+++ test/calculate/caclulate-epoch-ms-test.js
@@ -3,7 +3,13 @@ const { caclulateEpochMS } = require('../../src/calculate');
 const testValues = require('../test-values');
 
 describe('caclulateEpochMS', () => {
-  describe('empty array', () => {
+  describe('empty tests', () => {
+    it('should return result of Date.now() when given no arguments', () => {
+      const result = caclulateEpochMS();
+      const expected = Date.now();
+      assert.equal(result, expected);
+    });
+
     it('should return result of Date.now() when given empty array', () => {
       const result = caclulateEpochMS([]);
       const expected = Date.now();
@@ -11,8 +17,20 @@ describe('caclulateEpochMS', () => {
     });
   });
 
+
   describe('leap years', () => {
     testValues.leap.years.forEach(year => {
+      describe('when only year passed', () => {
+        it(`should return ${year.epoch} when passed only the year ${year.year}`, () => {
+          const array = [
+            year.year,
+          ];
+          const result = caclulateEpochMS(array);
+          const expect = year.epoch;
+          assert.equal(result.epochMs, expect);
+        });
+      });
+
       testValues.leap.remains.forEach(partial => {
         it(`#${partial.id} year: ${year.year}`, () => {
           const array = [
@@ -34,6 +52,17 @@ describe('caclulateEpochMS', () => {
 
   describe('non leap years', () => {
     testValues.noLeap.years.forEach(year => {
+      describe('when only year passed', () => {
+        it(`should return ${year.epoch} when passed only the year ${year.year}`, () => {
+          const array = [
+            year.year,
+          ];
+          const result = caclulateEpochMS(array);
+          const expect = year.epoch;
+          assert.equal(result.epochMs, expect);
+        });
+      });
+
       testValues.noLeap.remains.forEach(partial => {
         it(`#${partial.id} year: ${year.year}`, () => {
           const array = [
