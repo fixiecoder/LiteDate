a1664a695e8ce2720f8813771291d9823dc9505a
Colin Adams <colin.adams@actual-experience.com>
2018-03-28 14:01:36 +0100

    new format function

--- src/methods.js
+++ src/methods.js
@@ -22,91 +22,112 @@ const { prefixUnitZero, getOrdinal } = require('./helpers');
   Z ZZ      +12:00  Offset from UTC as +-HH:mm, +-HHmm, or Z
 
 */
-function format(formatString = '') {
-  const splitCharsRX = /[\/\s:-]/g;
-  const parts = formatString.split(/[\/\s:-]/);
-  const splitChars = formatString.match(splitCharsRX) || [];
 
-  let output = '';
+function convertFormatChunkIntoValue(chunk) {
+  switch(chunk) {
+    case 's':
+      return this.getSeconds();
 
-  for(let i = 0; i < parts.length; i += 1) {
-    let partReplaceString = '';
-    switch(parts[i]) {
-      case 's':
-        partReplaceString = this.getSeconds();
-        break;
-      case 'ss':
-        partReplaceString = prefixUnitZero(this.getSeconds());
-        break;
-      case 'm':
-        partReplaceString = this.getMinutes();
-        break;
-      case 'mm':
-        partReplaceString = prefixUnitZero(this.getMinutes());
-        break;
-      case 'H':
-        partReplaceString = this.getHours();
-        break;
-      case 'HH':
-        partReplaceString = prefixUnitZero(this.getHours());
-        break;
-      case 'h': {
-        const hours = this.getHours();
-        partReplaceString = hours > 12 ? hours - 12 : hours;
-        break;
-      }
-      case 'hh': {
-        const hours = this.getHours();
-        partReplaceString = prefixUnitZero(hours > 12 ? hours - 12 : hours);
-        break;
-      }
-      case 'x':
-        partReplaceString = this.getTime();
-        break;
-      case 'X':
-        partReplaceString = this.getTime() / 1000;
-        break;
-      case 'dddd':
-        partReplaceString = this.getDayOfWeek(this._TYPES.LONG);
-        break;
-      case 'ddd':
-        partReplaceString = this.getDayOfWeek(this._TYPES.SHORT);
-        break;
-      case 'Do':
-        partReplaceString = getOrdinal(this.getDate());
-        break;
-      case 'D':
-        partReplaceString = this.getDate();
-        break;
-      case 'DD':
-        partReplaceString = prefixUnitZero(this.getDate());
-        break;
-      case 'M':
-        partReplaceString = this.getMonth();
-        break;
-      case 'MM':
-        partReplaceString = prefixUnitZero(this.getMonth());
-        break;
-      case 'MMM':
-        partReplaceString = this.getMonthName(this._TYPES.SHORT);
-        break;
-      case 'MMMM':
-        partReplaceString = this.getMonthName(this._TYPES.LONG);
-        break;
-      case 'YY':
-        partReplaceString = this.getYear(this._TYPES.SHORT);
-        break;
-      case 'YYYY':
-        partReplaceString = this.getYear();
-        break;
-      default:
-        partReplaceString = parts[i];
-        break;
+    case 'ss':
+      return prefixUnitZero(this.getSeconds());
+
+    case 'm':
+      return this.getMinutes();
+
+    case 'mm':
+      return prefixUnitZero(this.getMinutes());
+
+    case 'H':
+      return this.getHours();
+
+    case 'HH':
+      return prefixUnitZero(this.getHours());
+
+    case 'h': {
+      const hours = this.getHours();
+      return hours > 12 ? hours - 12 : hours;
+
     }
-    output += `${partReplaceString}${splitChars[i] || ''}`;
+    case 'hh': {
+      const hours = this.getHours();
+      return prefixUnitZero(hours > 12 ? hours - 12 : hours);
+
+    }
+    case 'x':
+      return this.getTime();
+
+    case 'X':
+      return this.getTime() / 1000;
+
+    case 'dddd':
+      return this.getDayOfWeek(this._TYPES.LONG);
+
+    case 'ddd':
+      return this.getDayOfWeek(this._TYPES.SHORT);
+
+    case 'Do':
+      return getOrdinal(this.getDate());
+
+    case 'D':
+      return this.getDate();
+
+    case 'DD':
+      return prefixUnitZero(this.getDate());
+
+    case 'M':
+      return this.getMonth();
+
+    case 'MM':
+      return prefixUnitZero(this.getMonth());
+
+    case 'MMM':
+      return this.getMonthName(this._TYPES.SHORT);
+
+    case 'MMMM':
+      return this.getMonthName(this._TYPES.LONG);
+
+    case 'YY':
+      return this.getYear(this._TYPES.SHORT);
+
+    case 'YYYY':
+      return this.getYear();
+
+    default:
+      return chunk;
+
+  }
+}
+
+function format(formatString = '') {
+  const chunk = [];
+  const outputArray = [];
+  let previousChar = '';
+  const maxIterations = formatString.length - 1;
+  const ordinal = ['D', 'o'];
+  let skip = false;
+
+  for(let i = 0; i <= maxIterations; i += 1) {
+    const currentChar = formatString.charAt(i);
+    if((currentChar !== previousChar && i !== 0) || i === maxIterations) {
+      if((previousChar === ordinal[0] && currentChar === ordinal[1]) || i === maxIterations) {
+        chunk.push(currentChar);
+        skip = true;
+      }
+      // get the correct value for the chunk and push it into the outputArray
+      // reset the chunck back to length 0
+      outputArray.push(convertFormatChunkIntoValue.call(this, chunk.join('')));
+      chunk.length = 0;
+    }
+    if(skip === false) {
+      chunk.push(currentChar);
+    } else {
+      skip = false;
+    }
+    previousChar = currentChar;
   }
 
-  return output;
+
+  return outputArray.join('');
 }
 
 module.exports = {
