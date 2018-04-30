330e0bdfa867fc7cead34088cd26d78c4a3fefc4
Colin Adams <colin.adams@actual-experience.com>
2018-04-30 08:28:45 +0100

    updated tmp

--- src/tmp.js
+++ src/tmp.js
@@ -1,11 +1,12 @@
 const UTCDate = require('./index');
 const moment = require('moment');
 
-// const date = new UTCDate('2018-2-4', 'YYYY-D-M');
 // const date = new UTCDate();
 
-const d = moment([2018, 3, 7, 0, 0]).utc();
-console.log(d.toISOString());
+const d = moment([2018, 3, 7, 0, 0]);
+
+console.log(d);
+// console.log(d.toISOString());
 
 // console.log(date.format('YY/MM/DD h:mm:ss ddd MMM x X'));
 
@@ -13,15 +14,17 @@ console.log(d.toISOString());
 //
 // node --prof-process isolate-0x102801e00-v8.log
 //
-const year = 2018;
-const monthIndex = 3; // april
-const month = 4; // april
-const day = 7; // april
-const hour = 0; // april
-const minute = 0; // april
+// const year = 2018;
+// const monthIndex = 3; // april
+// const month = 4; // april
+// const day = 7; // april
+// const hour = 0; // april
+// const minute = 0; // april
 
-function jsUTC(bits) {
-  return new Date(Date.UTC(bits[0], bits[1], bits[2], bits[3], bits[4]));
-}
+// function jsUTC(bits) {
+//   return new Date(Date.UTC(bits[0], bits[1], bits[2], bits[3], bits[4]));
+// }
 
-console.log(jsUTC([year, monthIndex, day, hour, minute]));
+// const date = new UTCDate([year, month, day, hour, minute]);
+// console.log(date.toISOString())
+// console.log(jsUTC([year, monthIndex, day, hour, minute]));
